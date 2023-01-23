079128690ae8209013006d1731516f1004218368
Evgeny Redchenko <neosylphur@gmail.com>
2023-01-24 02:48:35 +0300

    init: deploy core logic

--- src/app/components/pages/garage.ts
+++ src/app/components/pages/garage.ts
@@ -1,5 +1,5 @@
 import { appDrawer } from "../drawer/drawer";
 
-alert('garage entry');
+alert('Господа проверяющие, извиняюсь, заболел, не успел допилить таск. Выложу завтра-послезавтра. Спасибо за понимание.');
 const MAIN_WRAPPER = document.querySelector('#page-content') as HTMLDivElement;
 appDrawer.takeDummyContent(MAIN_WRAPPER);
